b4109f1474d769d062e3a368d79ae39b342d420f
AritriBasu <AritriBasu@users.noreply.github.com>
2021-05-12 01:03:56 +0530

    fixed api img

--- public/js/national-news.js
+++ public/js/national-news.js
@@ -1,11 +1,8 @@
 var title=document.getElementsByClassName("card-img-title");
-var imgUrl=document.getElementsByClassName("news-card-image").src;
 var author=document.getElementsByClassName("card-author-content");
 var pubAt=document.getElementsByClassName("card-date-content");
 var description=document.getElementsByClassName("card-description");
 var moreUrl=document.getElementsByClassName("read-more");
-
-console.log("img object"+imgUrl);
 
 console.log("National News");
 
@@ -18,13 +15,7 @@ const getNationalNews = async () => {
 
 getNationalNews().then(NData => {
   console.log(NData);
-  for(var i=0;i<6;i++){
-  /**console.log("title", NData.articles[i].title);
-  console.log("description", NData.articles[i].description);
-  console.log("url", NData.articles[i].url);
-  console.log("image url", NData.articles[i].urlToImage);
-  console.log("published at(time)", NData.articles[i].publishedAt);
-  console.log("author", NData.articles[i].author);*/
+  for(var i=0;i<12;i++){
   
   var national_title=NData.articles[i].title;
   console.log(typeof(national_title));
@@ -36,16 +27,16 @@ getNationalNews().then(NData => {
   var national_url=NData.articles[i].url;
   moreUrl[i].href=national_url;
 
+  var imgUrl=document.getElementById(`img${i}`);
   var national_imgUrl=NData.articles[i].urlToImage;
   console.log(national_imgUrl);
-  //imgUrl[i].src=national_imgUrl;
+  imgUrl.src=national_imgUrl;
 
   var national_pubAt=NData.articles[i].publishedAt;
   pubAt[i].innerHTML=national_pubAt;
 
   var national_author=NData.articles[i].author;
   author[i].innerHTML=national_author;
-  //document.getElementById("total-cases").innerText =
-    //covidData.confirmed.value;
+  
   }//for loop
 });
--- public/js/state-news.js
+++ public/js/state-news.js
@@ -3,13 +3,11 @@
 //const API_KEY=process.env.NEWS_API_KEY;
 function getState(){
 var title=document.getElementsByClassName("card-img-title");
-var imgUrl=document.getElementsByClassName("news-card-image").src;
+
 var author=document.getElementsByClassName("card-author-content");
 var pubAt=document.getElementsByClassName("card-date-content");
 var description=document.getElementsByClassName("card-description");
 var moreUrl=document.getElementsByClassName("read-more");
-
-console.log("img object"+imgUrl);
 
 console.log("State News");
 
@@ -25,13 +23,7 @@ const getStateNews = async () => {
 
 getStateNews().then(SData => {
   console.log(SData);
-  for(var i=0;i<6;i++){
-  /**console.log("title", SData.articles[i].title);
-  console.log("description", SData.articles[i].description);
-  console.log("url", SData.articles[i].url);
-  console.log("image url", SData.articles[i].urlToImage);
-  console.log("published at(time)", SData.articles[i].publishedAt);
-  console.log("author", SData.articles[i].author);*/
+  for(var i=0;i<12;i++){
   
   var state_title=SData.articles[i].title;
   title[i].innerHTML=state_title;
@@ -41,10 +33,10 @@ getStateNews().then(SData => {
 
   var state_url=SData.articles[i].url;
   moreUrl[i].href=state_url;
-
+  
+  var imgUrl=document.getElementById(`img${i}`);
   var state_imgUrl=SData.articles[i].urlToImage;
-  console.log(state_imgUrl);
-  //imgUrl[i].src=national_imgUrl;
+  imgUrl.src=state_imgUrl;
 
   var state_pubAt=SData.articles[i].publishedAt;
   pubAt[i].innerHTML=state_pubAt;
